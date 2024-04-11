80c03d8112f8e6f21d17258e0fd36ec0954f043f
thdwoqor <celebarte01@gmail.com>
2024-04-11 14:23:27 +0900

    chore: init commit

---
+++ src/main/java/mafia/mafiatogether/MafiaTogetherApplication.java
@@ -0,0 +1,13 @@
+package mafia.mafiatogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MafiaTogetherApplication.class, args);
+    }
+
+}
