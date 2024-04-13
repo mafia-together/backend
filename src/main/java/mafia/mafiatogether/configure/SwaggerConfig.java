1ca50145723e90092a782257ee93d8abc0d544af
waterricecake <fruturum@nate.com>
2024-04-13 22:15:43 +0900

    git: conflict 해결

---
+++ src/main/java/mafia/mafiatogether/configure/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package mafia.mafiatogether.configure;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("TDM API 문서")
+                .description("TDM API 문서")
+                .version("1.0.0");
+    }
+}
