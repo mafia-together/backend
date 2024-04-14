6998e44c33d0049e9bd5738551052bcd8efb6311
thdwoqor <celebarte01@gmail.com>
2024-04-14 23:25:46 +0900

    test: 방 상태 확인 테스트 추가

--- src/test/java/mafia/mafiatogether/controller/RoomControllerTest.java
+++ src/test/java/mafia/mafiatogether/controller/RoomControllerTest.java
@@ -2,11 +2,16 @@ package mafia.mafiatogether.controller;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import mafia.mafiatogether.domain.RoomInfo;
+import mafia.mafiatogether.domain.RoomManager;
+import mafia.mafiatogether.domain.Status;
 import mafia.mafiatogether.service.dto.RoomCreateRequest;
 import mafia.mafiatogether.service.dto.RoomCreateResponse;
+import mafia.mafiatogether.service.dto.RoomStatusResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -15,6 +20,9 @@ import org.springframework.http.HttpStatus;
 @SuppressWarnings("NonAsciiCharacters")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class RoomControllerTest {
+
+    @Autowired
+    private RoomManager roomManager;
 
     @LocalServerPort
     private int port;
@@ -42,5 +50,23 @@ class RoomControllerTest {
 
         //then
         Assertions.assertThat(response.code()).isNotBlank();
+    }
+
+    @Test
+    void 방을_상태를_확인할_수_있다() {
+        //given
+        String code = roomManager.create(new RoomInfo(5, 1, 1, 1));
+
+        //when
+        RoomStatusResponse response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .when().get("/room/status?code=" + code)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(RoomStatusResponse.class);
+
+        //then
+        Assertions.assertThat(response.status()).isEqualTo(Status.WAIT);
     }
 }
