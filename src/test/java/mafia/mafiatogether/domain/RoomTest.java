f12fd194b7a752bd25f0fd44587e9fda6852a077
thdwoqor <celebarte01@gmail.com>
2024-04-29 20:51:42 +0900

    test: 직업 배정 테스트 작성

--- src/test/java/mafia/mafiatogether/domain/RoomTest.java
+++ src/test/java/mafia/mafiatogether/domain/RoomTest.java
@@ -2,39 +2,53 @@ package mafia.mafiatogether.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.EnumSet;
+import java.util.ArrayList;
+import java.util.List;
 import mafia.mafiatogether.domain.role.RoleSymbol;
 import org.junit.jupiter.api.Test;
 
 class RoomTest {
 
     @Test
-    void name() {
+    void 직업을_배정할_수_있다() {
         //given
-        Room room = Room.create(new RoomInfo(4, 1, 1, 1));
+        Room room = Room.create(new RoomInfo(5, 3, 0, 1));
         Player a = Player.create("A");
         Player b = Player.create("B");
         Player c = Player.create("C");
         Player d = Player.create("D");
+        Player e = Player.create("E");
+
         room.joinPlayer(a);
         room.joinPlayer(b);
         room.joinPlayer(c);
         room.joinPlayer(d);
+        room.joinPlayer(e);
 
         //when
         room.modifyStatus(Status.NIGHT);
 
         //then
-        EnumSet<RoleSymbol> roleSymbols = EnumSet.noneOf(RoleSymbol.class);
+
+        List<RoleSymbol> roleSymbols = new ArrayList<>();
         roleSymbols.add(a.getRoleSymbol());
         roleSymbols.add(b.getRoleSymbol());
         roleSymbols.add(c.getRoleSymbol());
         roleSymbols.add(d.getRoleSymbol());
+        roleSymbols.add(e.getRoleSymbol());
 
-        assertEquals(4, roleSymbols.size());
+        assertEquals(5, roleSymbols.size());
         assertTrue(roleSymbols.contains(RoleSymbol.CITIZEN));
         assertTrue(roleSymbols.contains(RoleSymbol.MAFIA));
         assertTrue(roleSymbols.contains(RoleSymbol.POLICE));
-        assertTrue(roleSymbols.contains(RoleSymbol.DOCTOR));
+
+        int mafiaPlayers = 0;
+        for (RoleSymbol symbol : roleSymbols) {
+            if (symbol == RoleSymbol.MAFIA) {
+                mafiaPlayers++;
+            }
+        }
+
+        assertEquals(3, mafiaPlayers);
     }
 }
