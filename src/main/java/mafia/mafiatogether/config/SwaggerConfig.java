66ddacc70303657acda29c24ca6fc163f103ba20
waterricecake <fruturum@nate.com>
2024-04-30 22:03:23 +0900

    refactor: controller Swagger 코드 제거

--- src/main/java/mafia/mafiatogether/config/SwaggerConfig.java
+++ src/main/java/mafia/mafiatogether/config/SwaggerConfig.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import java.util.List;
 import mafia.mafiatogether.controller.PlayerInfo;
 import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
@@ -29,7 +31,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(apiInfo());
+                .info(apiInfo())
+                .security(List.of(new SecurityRequirement().addList(AUTHORIZATION)));
     }
 
     private Info apiInfo() {
