c3307c4ef800e975d30a0768215d215ced109ef1
waterricecake <fruturum@nate.com>
2024-04-30 22:03:23 +0900

    refactor: controller Swagger 코드 제거

--- src/main/java/mafia/mafiatogether/controller/ChatController.java
+++ src/main/java/mafia/mafiatogether/controller/ChatController.java
@@ -1,15 +1,11 @@
 package mafia.mafiatogether.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mafia.mafiatogether.service.ChatService;
 import mafia.mafiatogether.service.dto.ChatRequest;
 import mafia.mafiatogether.service.dto.ChatResponse;
 import mafia.mafiatogether.service.dto.PlayerInfoDto;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,27 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/chat")
-@Tag(name = "02. Chat API", description = "채팅 조회 , 전송 API")
 public class ChatController {
 
     private final ChatService chatService;
 
 
     @GetMapping
-    @Operation(
-            description = "채팅 조회 API",
-            security = {@SecurityRequirement(name = HttpHeaders.AUTHORIZATION)}
-    )
     public ResponseEntity<List<ChatResponse>> findAllChat(@PlayerInfo PlayerInfoDto playerInfoDto) {
         return ResponseEntity.ok(chatService.findAllChat(playerInfoDto.code(), playerInfoDto.name()));
     }
 
 
     @PostMapping
-    @Operation(
-            description = "채팅 전송 API",
-            security = {@SecurityRequirement(name = HttpHeaders.AUTHORIZATION)}
-    )
     public ResponseEntity<Void> saveChat(
             @PlayerInfo PlayerInfoDto playerInfoDto,
             @RequestBody ChatRequest request
