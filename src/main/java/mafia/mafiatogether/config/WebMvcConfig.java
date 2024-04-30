5a953e7d65a2eec053ce07096ea5f6e4558bd870
waterricecake <fruturum@nate.com>
2024-04-30 14:09:40 +0900

    feat: cors 설정

--- src/main/java/mafia/mafiatogether/config/WebMvcConfig.java
+++ src/main/java/mafia/mafiatogether/config/WebMvcConfig.java
@@ -19,8 +19,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowCredentials(true)
                 .exposedHeaders(HttpHeaders.LOCATION);
         WebMvcConfigurer.super.addCorsMappings(registry);
