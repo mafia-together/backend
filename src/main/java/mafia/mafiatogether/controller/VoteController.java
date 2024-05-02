cdd78c3eeacf92a499c175578f8192dee7802f46
waterricecake <fruturum@nate.com>
2024-05-02 23:57:32 +0900

    feat: 투표 결과 조회 API 구현

--- src/main/java/mafia/mafiatogether/controller/VoteController.java
+++ src/main/java/mafia/mafiatogether/controller/VoteController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import mafia.mafiatogether.service.VoteService;
 import mafia.mafiatogether.service.dto.PlayerInfoDto;
 import mafia.mafiatogether.service.dto.VoteRequest;
+import mafia.mafiatogether.service.dto.VoteResultResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,13 @@ public class VoteController {
     ) {
         voteService.votePlayer(playerInfoDto.code(), playerInfoDto.name(), voteRequest.name());
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<VoteResultResponse> getResult(
+            @PlayerInfo final PlayerInfoDto playerInfoDto
+    ) {
+        final VoteResultResponse voteResultResponse = voteService.getResult(playerInfoDto.code());
+        return ResponseEntity.ok(voteResultResponse);
     }
 }
--- src/main/java/mafia/mafiatogether/service/VoteService.java
+++ src/main/java/mafia/mafiatogether/service/VoteService.java
@@ -3,6 +3,7 @@ package mafia.mafiatogether.service;
 import lombok.RequiredArgsConstructor;
 import mafia.mafiatogether.domain.Room;
 import mafia.mafiatogether.domain.RoomManager;
+import mafia.mafiatogether.service.dto.VoteResultResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +15,10 @@ public class VoteService {
     public void votePlayer(final String code, final String name, final String targetName) {
         final Room room = roomManager.findByCode(code);
         room.votePlayer(name, targetName);
+    }
+
+    public VoteResultResponse getResult(final String code) {
+        final Room room = roomManager.findByCode(code);
+        return new VoteResultResponse(room.getVoteResult());
     }
 }
