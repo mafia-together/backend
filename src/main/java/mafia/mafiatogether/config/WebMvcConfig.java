9df50619cbd7c76a029d657a3dd93d2be24c2f84
waterricecake <fruturum@nate.com>
2024-05-10 12:54:47 +0900

    fix: cors 에러 수정

--- src/main/java/mafia/mafiatogether/config/WebMvcConfig.java
+++ src/main/java/mafia/mafiatogether/config/WebMvcConfig.java
@@ -19,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://mafia-together","http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true)
                 .exposedHeaders(HttpHeaders.LOCATION);
