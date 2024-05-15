5d1e4f97ca488abf0b78ff379e69fb33fd8e1777
waterricecake <fruturum@nate.com>
2024-05-15 20:45:55 +0900

    feat: 모든 추가 상태 구현

--- src/main/java/mafia/mafiatogether/domain/status/NightIntroStatus.java
+++ src/main/java/mafia/mafiatogether/domain/status/NightIntroStatus.java
@@ -5,29 +5,27 @@ import java.time.Clock;
 import java.time.LocalDateTime;
 import mafia.mafiatogether.domain.Room;
 
-public class VoteStatus extends Status {
+public class NightIntroStatus extends Status {
 
-    private static final Long UNIT = 9999L;
+    public static final Long UNIT = 2999L;
 
-    private VoteStatus(final Timestamp startTime, final Timestamp endTime) {
+    private NightIntroStatus(Timestamp startTime, Timestamp endTime) {
         super(startTime, endTime);
     }
 
-    public static VoteStatus create(final Clock clock) {
+    protected static NightIntroStatus create(final Clock clock) {
         final Timestamp startTime = Timestamp.valueOf(LocalDateTime.now(clock));
         final Timestamp endTime = new Timestamp(startTime.getTime() + UNIT);
-        return new VoteStatus(startTime, endTime);
+        return new NightIntroStatus(startTime, endTime);
     }
 
     @Override
-    public Status getNextStatus(final Room room, final Clock clock) {
-        room.executeVote();
-        return VoteResultStatus.create(clock);
+    public Status getNextStatus(Room room, Clock clock) {
+        return NightStatus.create(clock);
     }
-
 
     @Override
     public StatusType getType() {
-        return StatusType.VOTE;
+        return StatusType.NIGHT_INTRO;
     }
 }
