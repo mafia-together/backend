882f4495fb1ea18e68a2920884ca6dc261c70e64
waterricecake <fruturum@nate.com>
2024-07-18 14:38:54 +0900

    refactor : controllerTest 추상 메서드 구

---
+++ src/test/java/mafia/mafiatogether/global/ControllerTest.java
@@ -0,0 +1,19 @@
+package mafia.mafiatogether.global;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public abstract class ControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+}
