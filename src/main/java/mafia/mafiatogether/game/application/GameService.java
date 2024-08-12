c4e248dd3ad465c9b8776806fa6ae150bd84f08d
waterricecake <fruturum@nate.com>
2024-08-12 13:15:52 +0900

    fix : Delete 저장 상태 검증 로직 변경

--- src/main/java/mafia/mafiatogether/game/application/GameService.java
+++ src/main/java/mafia/mafiatogether/game/application/GameService.java
@@ -38,7 +38,10 @@ public class GameService {
     private StatusType checkStatusChanged(final Game game) {
         game.setStatsSnapshot();
         final StatusType statusType = game.getStatusType(Clock.systemDefaultZone().millis());
-        if (game.isStatusChanged() && game.isNotDeleted()) {
+        if (game.isDeleted()){
+            return StatusType.WAIT;
+        }
+        if (game.isStatusChanged()) {
             gameRepository.save(game);
         }
         return statusType;
