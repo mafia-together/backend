3a0f24c7a7c385591bf1f0be33df95935dcfe214
jaeyeonkim <jakind@naver.com>
2024-09-22 17:52:38 +0900

    feat: Redis Message Listener Adapter로 등록

--- src/main/java/mafia/mafiatogether/config/WebsocketConfig.java
+++ src/main/java/mafia/mafiatogether/config/WebsocketConfig.java
@@ -1,7 +1,15 @@
 package mafia.mafiatogether.config;
 
 
+import lombok.RequiredArgsConstructor;
+import mafia.mafiatogether.chat.application.RedisMessageBroker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,17 +17,42 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompChannelInterceptor stompChannelInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
+        registry.addEndpoint("/stomp");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub");
         registry.enableSimpleBroker("/sub");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registry) {
+        registry.interceptors(stompChannelInterceptor);
+    }
+
+    @Bean
+    public MessageListenerAdapter redisMessageListenerAdapter(RedisMessageBroker redisMessageBroker) {
+        return new MessageListenerAdapter(redisMessageBroker, "onMessage");
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory redisConnectionFactory,
+            MessageListenerAdapter redisMessageListenerAdapter
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(redisMessageListenerAdapter, new PatternTopic("/sub/**"));
+
+        return container;
     }
 
 }
