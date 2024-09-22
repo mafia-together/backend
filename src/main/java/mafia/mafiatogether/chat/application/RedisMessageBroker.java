a397398c557ad5ce51956fff57b7f97c20ffa033
jaeyeonkim <jakind@naver.com>
2024-09-22 17:54:26 +0900

    feat: Publishing된 Message를 처리하는 RedisMessageBroker 추가

---
+++ src/main/java/mafia/mafiatogether/chat/application/RedisMessageBroker.java
@@ -0,0 +1,20 @@
+package mafia.mafiatogether.chat.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisMessageBroker implements MessageListener {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        simpMessagingTemplate.convertAndSend(new String(message.getChannel()), message.getBody());
+    }
+
+}
