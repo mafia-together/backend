ef72f62ebb3ad4fcb8128dc3d98fe1401cefd2cb
jaeyeonkim <jakind@naver.com>
2024-09-29 16:55:52 +0900

    feat: Message Type 추가

--- src/main/java/mafia/mafiatogether/chat/domain/Message.java
+++ src/main/java/mafia/mafiatogether/chat/domain/Message.java
@@ -3,20 +3,49 @@ package mafia.mafiatogether.chat.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import mafia.mafiatogether.chat.domain.vo.MessageType;
 
 import java.time.Clock;
+import java.util.Locale;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Message {
 
+    private static final String ENTER_MESSAGE_FORMAT = "%s님이 입장하셨습니다.";
+    private static final String LEAVE_MESSAGE_FORMAT = "%s님이 퇴장하셨습니다.";
+
     private String name;
     private String contents;
+    private MessageType messageType;
     private long timestamp;
 
-    public static Message of(String name, String content) {
-        return new Message(name, content, Clock.systemDefaultZone().millis());
+    public static Message ofChat(String name, String content) {
+        return new Message(
+                name,
+                content,
+                MessageType.CHAT,
+                Clock.systemDefaultZone().millis()
+        );
+    }
+
+    public static Message fromEnter(String name) {
+        return new Message(
+                name,
+                ENTER_MESSAGE_FORMAT.formatted(name),
+                MessageType.ENTER,
+                Clock.systemDefaultZone().millis()
+        );
+    }
+
+    public static Message fromLeave(String name) {
+        return new Message(
+                name,
+                LEAVE_MESSAGE_FORMAT.formatted(name),
+                MessageType.LEAVE,
+                Clock.systemDefaultZone().millis()
+        );
     }
 
 }
