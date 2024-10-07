59f416f61acb0db05b663f94c4f930592a1e3ccb
waterricecake <fruturum@nate.com>
2024-10-07 17:40:52 +0900

    feat : CORS origin dev 서버 추가

--- src/main/java/mafia/mafiatogether/common/config/WebMvcConfig.java
+++ src/main/java/mafia/mafiatogether/common/config/WebMvcConfig.java
@@ -1,13 +1,13 @@
 package mafia.mafiatogether.common.config;
 
-import java.util.List;
-
 import mafia.mafiatogether.common.resolver.PlayerArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -20,7 +20,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://mafia-together.com", "http://localhost:5173", "https://localhost:5173")
+                .allowedOrigins("https://dev.mafia-together.com", "https://mafia-together.com", "http://localhost:5173", "https://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowCredentials(true)
                 .exposedHeaders(HttpHeaders.LOCATION);
