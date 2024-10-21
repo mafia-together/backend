7577064f0041eec41736fe77acaa6ed07839ef9d
waterricecake <fruturum@nate.com>
2024-10-21 19:44:38 +0900

    refactor : testService 접근자 변경

--- src/test/java/mafia/mafiatogether/lobby/ui/RedisLockTestLobbyService.java
+++ src/test/java/mafia/mafiatogether/lobby/ui/RedisLockTestLobbyService.java
@@ -7,16 +7,16 @@ import mafia.mafiatogether.common.exception.GameException;
 import mafia.mafiatogether.lobby.domain.Lobby;
 import mafia.mafiatogether.lobby.domain.LobbyRepository;
 
-public class RedisLockTestLobbyService {
+class RedisLockTestLobbyService {
 
     private final LobbyRepository lobbyRepository;
 
-    public RedisLockTestLobbyService(LobbyRepository lobbyRepository) {
+    protected RedisLockTestLobbyService(LobbyRepository lobbyRepository) {
         this.lobbyRepository = lobbyRepository;
     }
 
     @RedisLock(key = "lobby")
-    public void waitAndInput(@RedisLockTarget String code, String name) throws InterruptedException {
+    protected void waitAndInput(@RedisLockTarget String code, String name) throws InterruptedException {
         Thread.sleep(50);
         final Lobby lobby = lobbyRepository.findById(code)
                 .orElseThrow(() -> new GameException(ExceptionCode.INVALID_NOT_FOUND_ROOM_CODE));
