d05fcf3d612b4f00cc42b3b2b731977972ae991c
waterricecake <fruturum@nate.com>
2024-10-21 18:44:24 +0900

    test : lobby 동시입장 테스트 구현

---
+++ src/test/java/mafia/mafiatogether/lobby/ui/RedisLockTestLobbyService.java
@@ -0,0 +1,27 @@
+package mafia.mafiatogether.lobby.ui;
+
+import mafia.mafiatogether.common.annotation.RedisLock;
+import mafia.mafiatogether.common.annotation.RedisLockTarget;
+import mafia.mafiatogether.common.exception.ExceptionCode;
+import mafia.mafiatogether.common.exception.GameException;
+import mafia.mafiatogether.lobby.domain.Lobby;
+import mafia.mafiatogether.lobby.domain.LobbyRepository;
+
+public class RedisLockTestLobbyService {
+
+    private final LobbyRepository lobbyRepository;
+
+    public RedisLockTestLobbyService(LobbyRepository lobbyRepository) {
+        this.lobbyRepository = lobbyRepository;
+    }
+
+    @RedisLock(key = "lobby")
+    public void waitAndInput(@RedisLockTarget String code, String name) throws InterruptedException {
+        Thread.sleep(50);
+        final Lobby lobby = lobbyRepository.findById(code)
+                .orElseThrow(() -> new GameException(ExceptionCode.INVALID_NOT_FOUND_ROOM_CODE));
+        System.out.println("Wait and Input");
+        lobby.joinPlayer(name);
+        lobbyRepository.save(lobby);
+    }
+}
