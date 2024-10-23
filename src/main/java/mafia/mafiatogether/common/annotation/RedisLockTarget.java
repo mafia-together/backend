0473f6f19a798dc0ff0dd16a1014f4e0d33e3c50
waterricecake <fruturum@nate.com>
2024-10-23 16:48:49 +0900

    docs : @exception을 통해 예외사항 정리

--- src/main/java/mafia/mafiatogether/common/annotation/RedisLockTarget.java
+++ src/main/java/mafia/mafiatogether/common/annotation/RedisLockTarget.java
@@ -3,7 +3,7 @@ package mafia.mafiatogether.common.annotation;
 import java.lang.annotation.*;
 
 /**
- * 이 어노테이션은 {@link mafia.mafiatogether.common.annotation.RedisLock}을 사용한 메서드의 String파라미터에 사용됩니다
+ * 이 어노테이션은 {@link mafia.mafiatogether.common.annotation.RedisLock}을 사용한 메서드의 String파라미터에 사용됩니다.
  * <p>
  * 작성자: waterricecake
  * <p>
