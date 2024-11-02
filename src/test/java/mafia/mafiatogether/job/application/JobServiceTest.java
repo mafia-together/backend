fd94c4c57b63f29a9e0ee399f7533ac7430a6d53
thdwoqor <celebarte01@gmail.com>
2024-11-03 00:51:37 +0900

    refactor: JobController 통일 및 테스트 수정

--- src/test/java/mafia/mafiatogether/job/application/JobServiceTest.java
+++ src/test/java/mafia/mafiatogether/job/application/JobServiceTest.java
@@ -18,9 +18,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SuppressWarnings("NonAsciiCharacters")
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class JobServiceTest {
 
     @Autowired
