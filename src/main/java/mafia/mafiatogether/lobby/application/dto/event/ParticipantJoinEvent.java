8d9101dc36b4759d1db84a74ba2ae8a1609f88d1
waterricecake <91263263+waterricecake@users.noreply.github.com>
2024-11-05 14:17:39 +0900

    hotfix : 참가자가 sse구독전에 요청하던 에러 수정 (#148)

--- src/main/java/mafia/mafiatogether/lobby/application/dto/event/ParticipantJoinEvent.java
+++ src/main/java/mafia/mafiatogether/lobby/application/dto/event/ParticipantJoinEvent.java
@@ -3,6 +3,7 @@ package mafia.mafiatogether.lobby.application.dto.event;
 import mafia.mafiatogether.lobby.domain.Lobby;
 
 public record ParticipantJoinEvent(
-        Lobby lobby
+        Lobby lobby,
+        String name
 ) {
 }
