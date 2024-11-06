ba1d0ab482d6c31e2dd38ae2634e0c1f4163f36f
Jae-Baek Song <83541246+thdwoqor@users.noreply.github.com>
2024-11-06 13:58:36 +0900

    feat: 직업 스킬 사용을 웹소켓으로 변경 (#146)
    * feat: 직업 스킬 사용을 웹소켓으로 변경
    
    * refactor: JobController 통일 및 테스트 수정

--- src/main/java/mafia/mafiatogether/common/interceptor/StompChannelInterceptor.java
+++ src/main/java/mafia/mafiatogether/common/interceptor/StompChannelInterceptor.java
@@ -1,5 +1,8 @@
 package mafia.mafiatogether.common.interceptor;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import mafia.mafiatogether.common.util.AuthExtractor;
 import org.springframework.context.annotation.Configuration;
@@ -11,16 +14,12 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Consumer;
-
 @Component
 @Configuration
 @RequiredArgsConstructor
 public class StompChannelInterceptor implements ChannelInterceptor {
 
-    private static final String SUBSCRIBE_FORMAT = "/sub/chat/%s";
+    private static final String SUBSCRIBE_FORMAT = "%s/%s";
     private static final String PUBLISHING_FORMAT = "%s/%s/%s";
 
     private final Map<StompCommand, Consumer<StompHeaderAccessor>> actionByCommand = Map.of(
@@ -44,7 +43,9 @@ public class StompChannelInterceptor implements ChannelInterceptor {
 
     private void consumeWhenSubscribe(StompHeaderAccessor headerAccessor) {
         String[] information = getInformation(headerAccessor);
-        headerAccessor.setDestination(SUBSCRIBE_FORMAT.formatted(information[0]));
+        String prefixUrl = headerAccessor.getDestination()
+                .substring(0, headerAccessor.getDestination().lastIndexOf('/'));
+        headerAccessor.setDestination(SUBSCRIBE_FORMAT.formatted(prefixUrl, information[0]));
     }
 
     private String[] getInformation(StompHeaderAccessor headerAccessor) {
--- src/main/java/mafia/mafiatogether/job/ui/JobController.java
+++ src/main/java/mafia/mafiatogether/job/ui/JobController.java
@@ -1,5 +1,7 @@
 package mafia.mafiatogether.job.ui;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import mafia.mafiatogether.common.annotation.PlayerInfo;
 import mafia.mafiatogether.common.resolver.PlayerInfoDto;
@@ -9,7 +11,11 @@ import mafia.mafiatogether.job.application.dto.response.JobResponse;
 import mafia.mafiatogether.job.application.dto.response.MafiaTargetResponse;
 import mafia.mafiatogether.job.application.dto.response.JobExecuteAbilityResponse;
 import mafia.mafiatogether.job.application.dto.response.JobResultResponse;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,10 +28,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class JobController {
 
     private final JobService jobService;
+    private final StringRedisTemplate stringRedisTemplate;
+    private final ObjectMapper objectMapper;
 
     @GetMapping("/my")
     public ResponseEntity<JobResponse> getJob(@PlayerInfo PlayerInfoDto playerInfoDto) {
         return ResponseEntity.ok(jobService.getPlayerJob(playerInfoDto.code(), playerInfoDto.name()));
+    }
+
+    @MessageMapping("/skill/{code}/{name}")
+    public void executeSkillV2(
+            @DestinationVariable("code") String code,
+            @DestinationVariable("name") String name,
+            @Payload JobExecuteAbilityRequest request
+    ) throws JsonProcessingException {
+        JobExecuteAbilityResponse response = jobService.executeSkill(code, name, request);
+        String message = objectMapper.writeValueAsString(response);
+        stringRedisTemplate.convertAndSend("/sub/jobs/skill/" + response.job().toLowerCase() + "/" + code, message);
     }
 
     @PostMapping("/skill")
