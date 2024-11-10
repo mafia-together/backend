e29573f1ece6fdf60569c4aab0fe2914c1b28f61
waterricecake <fruturum@nate.com>
2024-11-10 21:06:47 +0900

    refactor : aspect test method 모킹

--- src/test/java/mafia/mafiatogether/common/aspect/SseAspectTest.java
+++ src/test/java/mafia/mafiatogether/common/aspect/SseAspectTest.java
@@ -1,5 +1,6 @@
 package mafia.mafiatogether.common.aspect;
 
+import mafia.mafiatogether.common.annotation.PlayerInfo;
 import mafia.mafiatogether.common.domain.SseEmitterSession;
 import mafia.mafiatogether.common.resolver.PlayerInfoDto;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -11,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,11 +40,16 @@ class SseAspectTest {
 
         ProceedingJoinPoint joinPoint = mock(ProceedingJoinPoint.class);
         MethodSignature methodSignature = mock(MethodSignature.class);
-        Method method = SseAspectTestService.class.getDeclaredMethod("subscribe", PlayerInfoDto.class);
+
+        Method method = mock(Method.class);
+        Annotation[][] annotations = new Annotation[1][1];
+        annotations[0][0] = mock(PlayerInfo.class);
 
         given(joinPoint.getSignature()).willReturn(methodSignature);
         given(methodSignature.getMethod()).willReturn(method);
         given(joinPoint.getArgs()).willReturn(new Object[]{playerInfoDto});
+        given(method.getParameterAnnotations()).willReturn(annotations);
+
 
         SseEmitter sseEmitter = mock(SseEmitter.class);
         given(joinPoint.proceed()).willReturn(sseEmitter);
